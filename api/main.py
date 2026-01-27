f816e59ec63b954e6d35111f0166856f32a9dfef
nickgeorgemathew <148985027+nickgeorgemathew@users.noreply.github.com>
2026-01-27 23:34:18 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 from schemas import CustomerInput
-# from predict import predict_churn
+from predict import predict_churn
 import pandas as pd
 import csv
 from datetime import datetime
 
 app= FastAPI(title="Customer churn prediction")
 
+@app.get('/')
+def root():
+    return {"message":"Welcome to Customer Churn Prediction API"}
 @app.post('/predict')
 def get_prediction(customer_data:CustomerInput):
-    # x=predict_churn(customer_data)
-    # if not x:
+    x=predict_churn(customer_data)
+    if not x:
         return({
   "tenure": 3,
   "monthly_charges": 95.5,
@@ -18,5 +21,5 @@ def get_prediction(customer_data:CustomerInput):
   "contract_type": "Month-to-month",
   "payment_method": "Electronic check"
 })
-    # else:
-    #     return(x)
+    else:
+        return(x)
