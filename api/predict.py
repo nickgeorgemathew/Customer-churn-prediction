d05d9b319b547d0d0c8b5a111b39919926d13748
nickgeorgemathew <148985027+nickgeorgemathew@users.noreply.github.com>
2026-01-27 23:18:16 +0530

    Update predict.py

--- api/predict.py
+++ api/predict.py
@@ -1,10 +1,12 @@
-# import joblib
+import os
+import joblib
 
-# model=joblib.load("customer churn prediction/model_and_dev/model_v1.pkl")
-# preprocessor=joblib.load("customer churn prediction/model_and_dev/preprocessor.pkl")
+model_path = os.path.join(MODEL_DIR, "model_v1.pkl")
+model=joblib.load(model_path)
+preprocessor=joblib.load("customer churn prediction/model_and_dev/preprocessor.pkl")
 
-# def predict_churn(data):
-#     x=preprocessor.transform(data)
-#     predict=model.predict_proba(x)[0][1]
-#     return float(predict)
+def predict_churn(data):
+    x=preprocessor.transform(data)
+    predict=model.predict_proba(x)[0][1]
+    return float(predict)
 
