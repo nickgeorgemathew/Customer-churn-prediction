0c37ed49c2ccb2aa43ea97fdc7e973de8a5ca2b3
nickgeorgemathew <148985027+nickgeorgemathew@users.noreply.github.com>
2026-01-27 23:34:23 +0530

    Update predict.py

--- api/predict.py
+++ api/predict.py
@@ -1,9 +1,13 @@
 import os
 import joblib
 
+
+BASE_DIR = os.path.dirname(os.getcwd())  
+MODEL_DIR = os.path.join(BASE_DIR, "model_and_dev")
 model_path = os.path.join(MODEL_DIR, "model_v1.pkl")
+preprocessor_path=os.path.join(MODEL_DIR, "preprocessor.pkl")
 model=joblib.load(model_path)
-preprocessor=joblib.load("customer churn prediction/model_and_dev/preprocessor.pkl")
+preprocessor=joblib.load(preprocessor_path)
 
 def predict_churn(data):
     x=preprocessor.transform(data)
